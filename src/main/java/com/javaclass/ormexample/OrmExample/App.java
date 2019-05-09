3cf62a13a56bfb51db8704e87c4b0995c2dbd019
Suvash Khatiwada <suvashkhatiwada@gmail.com>
2019-05-08 20:28:44 -0400

    adding ORMExample on github

---
+++ src/main/java/com/javaclass/ormexample/OrmExample/App.java
@@ -0,0 +1,13 @@
+package com.javaclass.ormexample.OrmExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
